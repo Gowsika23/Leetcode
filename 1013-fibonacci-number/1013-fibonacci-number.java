d1fa1ac68f78b76dee4db3e8d24d9d0abb3b6e44
Gowsika <134724663+Gowsika23@users.noreply.github.com>
2025-08-04 14:59:10 +0530

    Time: 0 ms (100%), Space: 40.7 MB (27.03%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.java
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -3,10 +3,14 @@ class Solution {
         if(n<=1) return n;
         int[] dp = new int[n+1];
         dp[0] =0;
-        dp[1] = 1;
+        dp[1] = 1;int f=dp[0], s=dp[1];
+        int sum=f+s;
         for(int i=2;i<=n;i++){
-            dp[i] = dp[i-1] + dp[i-2];
+            int temp = f+s;
+            f=s;
+            s=temp;
+            sum = temp;
         }
-        return dp[n];        
+        return sum;        
     }
 }
