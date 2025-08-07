c9e1331161399e7bf94ab31e9ab76fe81d722468
Gowsika <134724663+Gowsika23@users.noreply.github.com>
2025-08-07 14:47:33 +0530

    Time: 0 ms (100%), Space: 44.7 MB (59.25%) - LeetHub

--- 1580-shuffle-the-array/1580-shuffle-the-array.java
+++ 1580-shuffle-the-array/1580-shuffle-the-array.java
@@ -1,12 +1,10 @@
 class Solution {
     public int[] shuffle(int[] nums, int n) {
-        int n2 = nums.length/2;int in=0;
+       int ind=0;
         int[] res = new int[nums.length];
         for(int i=0;i<n;i++){
-            res[in] = nums[i];
-            in++;
-            res[in] = nums[i+n];
-            in++;
+            res[ind++] = nums[i];
+            res[ind++] = nums[i+n];
         }
         return res;
     }
